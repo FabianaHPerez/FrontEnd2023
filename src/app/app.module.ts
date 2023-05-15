f4b0afd49dec503adffd781ab215cc276ff5ca5a
Fabiana Perez <fabiperez95@gmail.com>
2023-05-15 20:05:06 -0300

    Prueba 1
    Comentario 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from'@angular/common/http';
+import { HeaderComponent } from './component/header/header.component';
+import { InicioComponent } from './component/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/inicio/inicio.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit{
+  persona: persona = new persona("","","");
+  
+  constructor(public personaService: PersonaService) { }
+  
+  ngOnInit(): void {
+    this.persona = {
+      nombre: '-',
+      apellido: '-',
+      img:'-'
+    };
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
+  }
+
+}
